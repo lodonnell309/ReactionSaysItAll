e545dbae4a3f0cf4a6d8995bbabc9021d2c420f9
Liam O'Donnell <124012968+lodonnell309@users.noreply.github.com>
2025-04-08 20:25:35 -0500

    adding user functions

--- response_model/call_language_model.py
+++ response_model/call_language_model.py
@@ -1,5 +1,7 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
+import random
+import json
 
 model_path = "lodonnell32/ReactionSaysItAll"
 
@@ -12,7 +14,50 @@ def generate_response(input_text):
     outputs = model.generate(**inputs, max_length=50)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
+def get_random_msg():
+    with open("test_data.jsonl", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    random_index = random.choice(lines).strip()
+
+    entry = json.loads(random_index)
+
+    input_text = entry["input"]
+    context = input_text.split("context:")[1].strip() if "context:" in input_text else ""
+
+    return context
+
+
+def query_model(emotion='neutral',text_message='random'):
+    """
+    :param emotion:
+    Emotions include
+        - angry
+        - disgust
+        - fear
+        - happy
+        - neutral
+        - sad
+        - surprise
+    :param text_message:
+    :return: A thread of texts, if text_message is set to 'random' a random message will be "received" and be used
+    to generate the response
+    """
+    if str.lower(text_message) == 'random':
+        text_message = get_random_msg()
+
+    full_query = f'emotion: {emotion} | {text_message}'
+
+    print('Text Message Received:')
+    print(text_message)
+    print('Text Message Response:')
+    print(generate_response(full_query))
+
 
 if __name__ == "__main__":
     print(generate_response("emotion: fear | How are you feeling today?"))
     print(generate_response("emotion: sad | context: How is your day going?"))
+    print(generate_response("emotion: happy | context: Hi, my name is Liam"))
+    print('---------------------')
+
+    query_model(emotion='angry',text_message='random')
