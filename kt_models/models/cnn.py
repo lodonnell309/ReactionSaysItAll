1240f2ae82d58b13fed1b99d3ace2e385618f40f
Kelly Lee <kellychin79@gmail.com>
2025-04-12 00:05:25 -0400

    fix train() and tune cnn model (accuracy 0.55)

--- kt_models/models/cnn.py
+++ kt_models/models/cnn.py
@@ -11,6 +11,7 @@ class CNN(nn.Module):
                               kernel_size=3, # square kernel
                               stride=1,
                               padding=1)
+        self.bn = nn.BatchNorm2d(16)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, # square kernel
                                  stride=2)
@@ -20,6 +21,7 @@ class CNN(nn.Module):
                               kernel_size=3, # square kernel
                               stride=1,
                               padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2, # square kernel
                                  stride=2)
@@ -30,13 +32,14 @@ class CNN(nn.Module):
                             out_features = 7,
                             bias = True)
 
-        self.drop = nn.Dropout(p=0.25)
+        self.drop = nn.Dropout(p=0.5)
 
     def forward(self, x):
         # print(x.shape) # [64, 1, 48, 48]
 
         x = self.conv(x) # no need to reshape for convolution
         # print(x.shape) # [64, 16, 48, 48]
+        x = self.bn(x)
         x = self.relu(x)
         # print(x.shape) # [64, 16, 48, 48]
         x = self.pool(x)
@@ -44,6 +47,7 @@ class CNN(nn.Module):
 
         x = self.conv2(x) # no need to reshape for convolution
         # print(x.shape) # [64, 32, 24, 24]
+        x = self.bn2(x)
         x = self.relu2(x)
         # print(x.shape) # [64, 32, 24, 24]
         x = self.pool2(x)
